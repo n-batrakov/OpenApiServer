d52c8f480a7a468a304859d83d843ef8506a94e1
Никита Батраков <n.batrakov@itexpert.ru>
2018-07-03 14:22:48 +0300

    Add spec serving

--- src/App/Configuration/Startup.cs
+++ src/App/Configuration/Startup.cs
@@ -1,7 +1,7 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-using ITExpert.OpenApiServer.MockServer;
 using ITExpert.OpenApiServer.Utils;
 
 using JetBrains.Annotations;
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
 
 namespace ITExpert.OpenApiServer.Configuration
 {
@@ -24,6 +27,7 @@ namespace ITExpert.OpenApiServer.Configuration
         [UsedImplicitly]
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         [UsedImplicitly]
@@ -32,8 +36,42 @@ namespace ITExpert.OpenApiServer.Configuration
             var defaultDir = Path.Combine(env.ContentRootPath, "Specs");
             var specsDir = Configuration.GetValue("specs", defaultDir);
             var specs = OpenApiDocumentsProvider.GetDocuments(specsDir).ToArray();
+            WriteSpecs("wwwroot", specs);
 
-            app.UseMockServer(specs);
+            //app.UseMockServer(specs);
+
+            app.UseFileServer(new FileServerOptions
+                              {
+                                      EnableDirectoryBrowsing = true,
+                                      DirectoryBrowserOptions = { RequestPath = ""}
+                              });
+        }
+
+        private static void WriteSpecs(string dir, IEnumerable<OpenApiDocument> specs)
+        {
+            foreach (var spec in specs)
+            {
+                WriteOneSpec(spec);
+            }
+
+            void WriteOneSpec(OpenApiDocument spec)
+            {
+                var path = GetSpecFilePath(dir, spec.Info.Version, spec.Info.Title);
+                var content = spec.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0);
+
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                using (var writer = File.CreateText(path))
+                {
+                    writer.Write(content);
+                }
+            }
+        }
+
+        private static string GetSpecFilePath(string dir, string apiVersion, string apiTitle)
+        {
+            var name = apiTitle.Replace(" ", "");
+            return Path.Combine(dir, name, apiVersion, "swagger.json");
         }
     }
 }
