515233443d06c86f181ee1f163b91edeccc5a0a1
Никита Батраков <n.batrakov@itexpert.ru>
2018-07-31 16:49:04 +0300

    Add Load command

---
+++ src/Tools/Commands/Load/LoadCommand.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using ITExpert.OpenApi.Server.Utils;
+using ITExpert.OpenApi.Tools.Commands.Server.DocumentProviders;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.OpenApi.Models;
+
+namespace ITExpert.OpenApi.Tools.Commands.Load
+{
+    public class LoadCommand
+    {
+        private LoadCommandOptions Options { get; }
+        private ILogger Logger { get; }
+        private IHttpClientFactory ClientFactory { get; }
+
+        public LoadCommand(LoadCommandOptions options)
+        {
+            Options = options;
+            ClientFactory = new HttpClientFactory();
+            Logger = new ConsoleLogger("SpecLoader", (s, level) => true, includeScopes: false);
+        }
+
+        public int Execute()
+        {
+            if (Options.Sources == null || Options.Sources.Length == 0)
+            {
+                PrintArgumentError();
+                return 1;
+            }
+
+            PrintStart();
+
+            var specs = Options.Sources.SelectMany(GetSpecs);
+            var tasks = specs.Select(WriteSpec).ToArray();
+            Task.WaitAll(tasks);
+
+            PrintFinish();
+            return 0;
+        }
+
+        private IEnumerable<OpenApiDocument> GetSpecs(string source)
+        {
+            var provider = GetProvider(source);
+            return provider.GetDocuments();
+            
+            IOpenApiDocumentProvider GetProvider(string url)
+            {
+                if (Options.TreatSourcesAsDiscoveryFiles)
+                {
+                    return new DiscoveryOpenApiDocumentProvider(ClientFactory, url, Options.DiscoveryKey, Logger);
+                }
+                else
+                {
+                    return new WebOpenApiDocumentProvider(ClientFactory, url);
+                }
+            }
+        }
+
+        private async Task WriteSpec(OpenApiDocument spec)
+        {
+            Directory.CreateDirectory(Options.OutputPath);
+
+            var filename = $"{spec.Info.GetServiceName()}.v{spec.Info.GetMajorVersion()}.json";
+            var path = Path.Join(Options.OutputPath, filename);
+            var text = OpenApiSerializer.Serialize(spec);
+
+            using (var writer = File.CreateText(path))
+            {
+                await writer.WriteAsync(text).ConfigureAwait(false);
+            }
+        }
+
+        private void PrintStart()
+        {
+            Console.WriteLine();
+            Console.WriteLine("OpenAPI specification loading is launched.");
+
+            Console.WriteLine();
+            Console.WriteLine("Parameters:");
+            Console.WriteLine($"* Sources: {string.Join(", ", Options.Sources)}");
+            Console.WriteLine($"* Output directory: {Path.GetFullPath(Options.OutputPath)}");
+            Console.WriteLine(
+                    $"* Discovery: {(Options.TreatSourcesAsDiscoveryFiles ? "Yes" : "No")} (key: '{Options.DiscoveryKey}')");
+            
+            Console.WriteLine();
+        }
+
+        private static void PrintFinish()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+        }
+
+        private static void PrintArgumentError()
+        {
+            Console.WriteLine("Load command requires at least one argument to be specified.");
+            Console.WriteLine("See 'oas load --help'.");
+        }
+
+        private class HttpClientFactory : IHttpClientFactory
+        {
+            public HttpClient CreateClient(string name) => new HttpClient();
+        }
+    }
+}
---
+++ src/Tools/Commands/Load/LoadCommandOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ITExpert.OpenApi.Tools.Commands.Load
+{
+    public class LoadCommandOptions
+    {
+        public string[] Sources { get; set; }
+        public string OutputPath { get; set; }
+        public bool TreatSourcesAsDiscoveryFiles { get; set; }
+        public string DiscoveryKey { get; set; }
+    }
+}
