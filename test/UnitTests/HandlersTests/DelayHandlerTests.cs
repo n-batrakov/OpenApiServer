073ba7d593f10d24ebef0d4b42b6f5718104e1a0
Никита Батраков <n.batrakov@itexpert.ru>
2018-10-15 17:23:51 +0300

    Test DelayHandler

---
+++ test/UnitTests/HandlersTests/DelayHandlerTests.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+
+using OpenApiServer.Core.MockServer.Handlers.Defaults;
+
+using UnitTests.Utils;
+
+using Xunit;
+
+namespace UnitTests.HandlersTests
+{
+    public class DelayHandlerTests
+    {
+        [Fact]
+        public void CanDelayRequest()
+        {
+            const int delayMilliseconds = 10;
+            var sut = new DelayHandler(new DelayHandler.Options {Value = delayMilliseconds});
+            var ctx = RouteContextBuilder.FromUrl("/").Build();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            
+            var actual = sut.HandleAsync(ctx).Result;
+
+            stopwatch.Stop();
+
+            Assert.Null(actual);
+            Assert.InRange(stopwatch.ElapsedMilliseconds, delayMilliseconds, delayMilliseconds * 10);
+        }
+    }
+}
