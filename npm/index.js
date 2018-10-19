c697c13ce7cd35b8e5b8371c54bb5b29eec8f63a
Никита Батраков <n.batrakov@itexpert.ru>
2018-10-19 18:15:13 +0300

    Set up CI

---
+++ npm/index.js
@@ -0,0 +1,19 @@
+var binwrap = require("binwrap");
+var path = require("path");
+
+var packageInfo = require(path.join(__dirname, "package.json"));
+var version = packageInfo.version;
+
+var root = 'https://github.com/Lasmera/OpenApiServer/releases/download/' + version + '/oas-' + version + '-';
+
+module.exports = binwrap({
+  dirname: __dirname,
+  binaries: [
+    "oas"
+  ],
+  urls: {
+    "darwin-x64": root + "osx-x64.tar.gz",
+    "linux-x64": root + "linux-x64.tar.gz",
+    "win32-x64": root + "win-x64.zip"
+  }
+});
